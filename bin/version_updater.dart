aa068de36c45e2a3580561f85f757b826770b8a4
xM1haix <kilommm4@gmail.com>
2024-08-14 13:02:40 +0300

    making it run in project

--- bin/version_updater.dart
+++ bin/version_updater.dart
@@ -2,33 +2,21 @@ import 'dart:io';
 
 import 'package:yaml/yaml.dart';
 
-void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Please provide the path to your HTML file.');
+void main() {
+  final Directory currentDir = Directory.current;
+  final pubspecFile = File('${currentDir.path}/pubspec.yaml');
+  if (!pubspecFile.existsSync()) {
+    print('Error: pubspec.yaml not found in the current directory.');
     exit(1);
   }
-
-  final htmlFilePath = arguments[0];
-
-  // Read pubspec.yaml
-  final pubspec = File('pubspec.yaml');
-  final content = pubspec.readAsStringSync();
-  final doc = loadYaml(content);
-
-  // Extract version
-  final version = doc['version'] as String;
-
-  // Read HTML file
-  final htmlFile = File(htmlFilePath);
-  var htmlContent = htmlFile.readAsStringSync();
-
-  // Replace the version in the script tag
-  final updatedHtmlContent = htmlContent.replaceFirst(
+  final webIndexFile = File('${currentDir.path}/web/index.html');
+  if (!webIndexFile.existsSync()) {
+    print('Error: web/index.html not found in the current directory.');
+    exit(1);
+  }
+  final version = loadYaml(pubspecFile.readAsStringSync())['version'] as String;
+  webIndexFile.writeAsStringSync(webIndexFile.readAsStringSync().replaceFirst(
       RegExp(r'flutter_bootstrap\.js\?v=\d+\.\d+\.\d+'),
-      'flutter_bootstrap.js?v=$version');
-
-  // Write the updated content back to the HTML file
-  htmlFile.writeAsStringSync(updatedHtmlContent);
-
-  print('Updated version to $version in $htmlFilePath');
+      'flutter_bootstrap.js?v=$version'));
+  print('Updated version to $version in ${webIndexFile.path}');
 }
