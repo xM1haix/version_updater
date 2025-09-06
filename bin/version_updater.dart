d94a5be1ed7b8ccb6831ed3530dfc964baae905c
xM1haix <kilommm4@gmail.com>
2025-09-06 16:58:53 +0300

    adding custom lints and finxing warning and info

--- bin/version_updater.dart
+++ bin/version_updater.dart
@@ -1,22 +1,25 @@
-import 'dart:io';
+import "dart:io";
 
-import 'package:yaml/yaml.dart';
+import "package:yaml/yaml.dart";
 
 void main() {
-  final Directory currentDir = Directory.current;
-  final pubspecFile = File('${currentDir.path}/pubspec.yaml');
+  final currentDir = Directory.current;
+  final pubspecFile = File("${currentDir.path}/pubspec.yaml");
   if (!pubspecFile.existsSync()) {
-    print('Error: pubspec.yaml not found in the current directory.');
+    print("Error: pubspec.yaml not found in the current directory.");
     exit(1);
   }
-  final webIndexFile = File('${currentDir.path}/web/index.html');
+  final webIndexFile = File("${currentDir.path}/web/index.html");
   if (!webIndexFile.existsSync()) {
-    print('Error: web/index.html not found in the current directory.');
+    print("Error: web/index.html not found in the current directory.");
     exit(1);
   }
-  final version = loadYaml(pubspecFile.readAsStringSync())['version'] as String;
-  webIndexFile.writeAsStringSync(webIndexFile.readAsStringSync().replaceFirst(
-      RegExp(r'flutter_bootstrap\.js\?v=\d+\.\d+\.\d+'),
-      'flutter_bootstrap.js?v=$version'));
-  print('Updated version to $version in ${webIndexFile.path}');
+  final version = loadYaml(pubspecFile.readAsStringSync())["version"] as String;
+  webIndexFile.writeAsStringSync(
+    webIndexFile.readAsStringSync().replaceFirst(
+          RegExp(r"flutter_bootstrap\.js\?v=\d+\.\d+\.\d+"),
+          "flutter_bootstrap.js?v=$version",
+        ),
+  );
+  print("Updated version to $version in ${webIndexFile.path}");
 }
